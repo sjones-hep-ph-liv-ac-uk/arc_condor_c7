c4c7e18f14d998df328ab48d473e08490b18d1b1
root <root@mike.ph.liv.ac.uk>
2017-12-20 15:13:00 +0000

    Dummy password, also fewer params

--- puppet/environments/production/modules/condor_c7_svr/manifests/params.pp
+++ puppet/environments/production/modules/condor_c7_svr/manifests/params.pp
@@ -1,7 +1,4 @@
 class condor_c7_svr::params(
-    $condor_head_i  = 'undefined_condor_head_i',
-    $condor_head_hep  = 'undefined_condor_head_hep',
     $condor_daemon_list = 'undefined_condor_daemon_list',
-    $condor_cman = 'undefined_condor_cman',
   ) 
   { }
